dd8082e1360ed4a02602af56f2a183ca35dfb8f5
pesantrenriqa-boop <pesantrenriqa@gmail.com>
2026-08-15 22:48:06 +0700

    feat(j2-p009): single focus ghain renderer

---
+++ tools/render_qurbata_jilid2_p009_v1_ghain_form_drill.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from render_qurbata_jilid2_single_form_focus import run
+if __name__=='__main__':
+    raise SystemExit(run(9,'غ','غَرَبَ','فقكلمنيه','V1-GHAIN-SINGLE-FORM'))
